98560a3c33e29add22c55662f6488ee28d796998
paolo marzolo <pollo.marzo@gmail.com>
2024-05-07 13:10:41 +0200

    moving to double MSO instead of single MSO with different behaviors

--- code/consts.py
+++ code/consts.py
@@ -3,9 +3,11 @@ class Constants:
         ANFs2SBCs = 2.0
         ANFs2GBCs = 1.0
         SBCs2MSO = 1
-        SBCs2MSO_inh = [0, -30]
+        SBCs2MSO_inh = 0
+        SBCs2inhMSO_inh = -30
         SBCs2LSO = 16.0
-        MNTBCs2MSO = [0, -30]
+        MNTBCs2MSO = -30
+        MNTBCs2inhMSO = -30
         GBCs2MNTBCs = 16.0
         MNTBCs2LSO = -2.0
 
@@ -14,12 +16,13 @@ class Constants:
         ANFs2GBCs = 20
 
     n_ANFs = 35000
-    n_battery = len(SYN_WEIGHTS.MNTBCs2MSO)
+    # n_battery = len(SYN_WEIGHTS.MNTBCs2MSO)
     SBCs2MSOs = int(POP_CONN.ANFs2GBCs / POP_CONN.ANFs2SBCs)
     SBCs2LSOs = int(POP_CONN.ANFs2GBCs / POP_CONN.ANFs2SBCs)
     n_SBCs = int(n_ANFs / POP_CONN.ANFs2SBCs)
     n_GBCs = int(n_ANFs / POP_CONN.ANFs2GBCs)
-    n_MSOs = n_GBCs * n_battery
+    n_MSOs = n_GBCs  # * n_battery
+    n_inhMSOs = n_GBCs
     V_m = V_reset = -70  # mV
     C_m_sbc = 3
     C_m_gcb = 2
