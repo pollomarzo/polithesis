e9a81040688f4218dc3996da0b064de3a76af97e
paolo marzolo <pollo.marzo@gmail.com>
2024-05-16 16:05:00 +0200

    ran a couple tests. diagrams mostly are as expected

--- code/cochlea-to-LSO.py
+++ code/cochlea-to-LSO.py
@@ -1,10 +1,10 @@
 from SLModel import SLModel
 from utils import logger
 from generate_DB import load_saved_anf_as_nestgen
-from consts import Constants as C, Paths as P, save_current_conf
+from consts import Paths, save_current_conf, Parameters
 from pathlib import Path
 import brian2
-import pickle
+import dill
 import datetime
 import nest
 import nest.voltage_trace
@@ -22,36 +22,48 @@ SOUND = "tone_1.kHz"
 anf_spikes = load_saved_anf_as_nestgen([SOUND])[SOUND]
 logger.info("...loaded saved anfs!")
 
-result = {}
-
-angle_to_rate = {}
-"""
-{
-    90: {n_spikes_r_lso, n_spikes_l_lso, n_spikes_r_mso, n_spikes_l_mso},
-    105: {n_spikes_r_lso, n_spikes_l_lso, n_spikes_r_mso, n_spikes_l_mso}
-    ...
-}
-"""
 
 logger.info("beginning to cycle through angles...")
-for angle, binaural_anf in anf_spikes.items():
-    nest.ResetKernel()
-    logger.info(f"\tcurrent angle: {angle}")
-    model = SLModel(binaural_anf)
-    logger.info(f"\t\tmodel initialized. begin simulation...")
-    model.simulate(TIME_SIMULATION)
-    logger.info(f"\t\t...simulation complete. collecting results...")
-    angle_to_rate[angle] = model.analyze()
-    break
+params_normal = Parameters()
+params_modified = Parameters()
+params_modified.SYN_WEIGHTS.SBCs2MSO_inh = 0
+params_modified.SYN_WEIGHTS.MNTBCs2MSO = 0
 
+print(params_modified.SYN_WEIGHTS.MNTBCs2MSO)
+print(params_normal.SYN_WEIGHTS.MNTBCs2MSO)
 
-result_file = Path(P.RESULTS_DIR).joinpath(
-    f"{SOUND}_{datetime.datetime.now().isoformat()[:-5]}.pic"
-)
+for params, model_key, model_desc in zip(
+    [params_normal, params_modified],
+    ["default", "no_inh"],
+    [SLModel.name, "inhibition to MSO turned off"],
+):
+    logger.info(f"\tnow working on model {model_key}")
+    result = {}
+    angle_to_rate = {}
+    """
+    {
+        90: {n_spikes_r_lso, n_spikes_l_lso, n_spikes_r_mso, n_spikes_l_mso},
+        105: {n_spikes_r_lso, n_spikes_l_lso, n_spikes_r_mso, n_spikes_l_mso}
+        ...
+    }
+    """
+    for angle, binaural_anf in anf_spikes.items():
+        nest.ResetKernel()
+        logger.info(f"\t\tcurrent angle: {angle}")
+        model = SLModel(params, binaural_anf)
+        model.name = model_key
+        logger.info(f"\t\t\tmodel initialized. begin simulation...")
+        model.simulate(TIME_SIMULATION)
+        logger.info(f"\t\t\t...simulation complete. collecting results...")
+        angle_to_rate[angle] = model.analyze()
 
-result["conf"] = save_current_conf(model)
-result["angle_to_rate"] = angle_to_rate
+    result_file = Path(Paths.RESULTS_DIR).joinpath(
+        f"{SOUND}-{model_key}-{datetime.datetime.now().isoformat()[:-5]}.pic"
+    )
 
-logger.info(f"saving results to {result_file.absolute()}...")
-with open(result_file, "wb") as f:
-    pickle.dump(result, f)
+    result["conf"] = save_current_conf(model, params)
+    result["angle_to_rate"] = angle_to_rate
+
+    logger.info(f"\tsaving results for {model_key} to {result_file.absolute()}...")
+    with open(result_file, "wb") as f:
+        dill.dump(result, f)
